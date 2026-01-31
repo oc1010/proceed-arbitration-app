71ebb64170c3bae8fe5427f375b0f3c4f6b460ff
oc1010 <ottacurin@gmail.com>
2026-01-31 16:47:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from db import create_new_case, get_active_case_id, load_full_config, verify_case_access, get_all_cases_metadata, db
+from db import create_new_case, get_active_case_id, load_full_config, verify_case_access, get_all_cases_metadata, db, reset_database
 
 st.set_page_config(page_title="PROCEED | Arbitration Cloud", layout="wide")
 
